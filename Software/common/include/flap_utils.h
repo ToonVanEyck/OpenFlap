39cba503c076bb17d5ea7b2de437bb404ad72bf9
Toon Van Eyck <toonvaneyck@gmail.com>
2023-07-15 16:07:42 +0000

    Added a devcontainer supporting ESP-IDF 5.1. Fixed a bug in the offset and vtrim module view.

--- Software/common/include/flap_utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef FLAP_UTILS_H
-#define FLAP_UTILS_H
-
-int strLen_utf8(char *s);
-int charLen_utf8(char *s);
-char * strIndex_utf8(char *s, int index);
-char * strNext_utf8(char *s);
-
-#endif

4a474326a726e8c3af92e91cb2637a1167e47845
Toon Van Eyck <toonvaneyck@gmail.com>
2024-09-11 21:53:22 +0000

    splitting devcontainers, added devcontainer for kibot testing

---
+++ .devcontainer/bashrc_addons_add.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Define the line to be added
+line_to_add='[ -f $HOME/.bashrc_devcontainer_addons ] && . $HOME/.bashrc_devcontainer_addons'
+
+# Check if the line is already in .bashrc
+if ! grep -Fxq "$line_to_add" "$HOME/.bashrc"; then
+    # If not, add the line to .bashrc
+    echo "" >> "$HOME/.bashrc"
+    echo "# Load bashrc devcontainer addons" >> "$HOME/.bashrc"
+    echo "$line_to_add" >> "$HOME/.bashrc"
+fi

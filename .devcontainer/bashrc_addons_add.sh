2cf915f75c26cde422078a358edf37631c792cb2
Toon Van Eyck <toonvaneyck@gmail.com>
2024-09-19 13:30:36 +0000

    Test workflow using devcontainer

--- .devcontainer/bashrc_addons_add.sh
+++ .devcontainer/bashrc_addons_add.sh
@@ -3,6 +3,10 @@
 # Define the line to be added
 line_to_add='[ -f $HOME/.bashrc_devcontainer_addons ] && . $HOME/.bashrc_devcontainer_addons'
 
+# Check if .bashrc exists and is writable
+if [ ! -f "$HOME/.bashrc" ] || [ ! -w "$HOME/.bashrc" ]; then
+    exit 0
+fi
 # Check if the line is already in .bashrc
 if ! grep -Fxq "$line_to_add" "$HOME/.bashrc"; then
     # If not, add the line to .bashrc
---
+++ hardware/ci-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+pushd module/side_panel/src
+kibot
+
+exit 1

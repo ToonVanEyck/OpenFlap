3cdb218784526dbcb364640224d8e67f5e1bd7af
Toon Van Eyck <toonvaneyck@gmail.com>
2024-09-20 20:04:31 +0000

    set fetch depth in workflow

--- hardware/ci-script.sh
+++ hardware/ci-script.sh
@@ -1,7 +1,42 @@
 #!/bin/bash
-set -e
 
-pushd module/side_panel/src
-kibot
+# Parse arguments
+dry_run=false
+if [[ "$1" == "--dry-run" ]]; then
+    dry_run=true
+fi
 
-exit 1
+# Set variables based on the argument
+if $dry_run; then
+    message="Validating"
+    kibot_preflight_only="-i"
+else
+    message="Generating"
+    kibot_preflight_only=""
+fi
+
+# Declare modules
+declare -A modules=(
+    ["Side Panel"]="module/side_panel/src"
+    ["Top Connector"]="top_connector/src"
+)
+
+# Run KiBot for each module
+for name in "${!modules[@]}"; do
+    path=${modules[$name]}
+    
+    echo -e "\033[1;36m$message $name\033[0m"
+    pushd $path > /dev/null
+    kibot $kibot_preflight_only
+    if [[ $? -ne 0 ]]; then
+        all_success=false
+    fi
+    popd > /dev/null
+done
+
+# Return success only if there were no failures
+if $all_success; then
+    exit 0
+else
+    exit 1
+fi

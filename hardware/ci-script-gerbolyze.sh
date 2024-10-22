fa0f8a94c62a8c6c6c74409c1fccec9139d0f1fc
Toon Van Eyck <toonvaneyck@gmail.com>
2024-10-22 20:38:28 +0000

    update gerbolyzer ci script to build out of source

--- hardware/ci-script-gerbolyze.sh
+++ hardware/ci-script-gerbolyze.sh
@@ -19,11 +19,15 @@ if [ "$(hostname)" != "gerbolyze" ]; then
     exit 1
 fi
 
+BASE_DIR=/workspaces/OpenFlap
+FLAPS_SOURCE_DIR=${BASE_DIR}/hardware/module/flaps
+FLAPS_BUILD_DIR=${BASE_DIR}/build/flaps
+
 # Try generating the flaps
-pushd "module/flaps" > /dev/null
+mkdir -p ${FLAPS_BUILD_DIR}
+rm -rf ${FLAPS_BUILD_DIR}/*
 echo -e "\033[1;36m$message Flaps\033[0m"
 echo -e "\033[96mStep 1: Generate .zip with browser based flap generator\033[0m"
-python3 browser_based_flap_generator/test/browser_based_flap_generator_test.py $(realpath browser_based_flap_generator/index.html) $(realpath build/)
+python3 ${FLAPS_SOURCE_DIR}/browser_based_flap_generator/test/browser_based_flap_generator_test.py ${FLAPS_SOURCE_DIR}/browser_based_flap_generator/index.html ${FLAPS_BUILD_DIR}
 echo -e "\033[96mStep 2: Convert .zip with gerbolyze\033[0m"
-./svg_to_gerber.sh build/flaps.zip build
-popd > /dev/null
+${FLAPS_SOURCE_DIR}/svg_to_gerber.sh ${FLAPS_BUILD_DIR}/flaps.zip ${FLAPS_BUILD_DIR}

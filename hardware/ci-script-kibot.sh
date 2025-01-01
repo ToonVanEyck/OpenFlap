9eac5a5089d58e326c48e5238a2c74ed50cba067
Toon Van Eyck <toonvaneyck@gmail.com>
2025-01-01 12:52:47 +0000

    Updated controller footprint and added testpads

--- hardware/ci-script-kibot.sh
+++ hardware/ci-script-kibot.sh
@@ -36,16 +36,26 @@ declare -A modules=(
 # Run KiBot for each module
 for name in "${!modules[@]}"; do
     path=${modules[$name]}
-    
     echo -e "\033[1;36m$message $name\033[0m"
-    kibot -c $(realpath $HW_SOURCE_DIR/$path/*.kibot.yaml) -e $(realpath $HW_SOURCE_DIR/$path/*.kicad_sch) -b $(realpath $HW_SOURCE_DIR/$path/*.kicad_pcb) -d $HW_BUILD_DIR/$(basename $(dirname $path))
+
+    # Compare title blocks
+    grep -A 8 "(title_block" $(realpath $HW_SOURCE_DIR/$path/*.kicad_sch) > /tmp/sch_title_block.txt
+    grep -A 8 "(title_block" $(realpath $HW_SOURCE_DIR/$path/*.kicad_pcb) > /tmp/pcb_title_block.txt
+    echo "- Comparing Title Blocks"
+    if ! diff /tmp/sch_title_block.txt /tmp/pcb_title_block.txt --strip-trailing-cr; then
+        echo -e "\e[31mThe title blocks are different.\e[0m"
+        all_success=false
+    fi
+
+    # Run KiBot
+    kibot -c $(realpath $HW_SOURCE_DIR/$path/*.kibot.yaml) -e $(realpath $HW_SOURCE_DIR/$path/*.kicad_sch) -b $(realpath $HW_SOURCE_DIR/$path/*.kicad_pcb) -d $HW_BUILD_DIR/$(basename $(dirname $path)) $kibot_preflight_only
     if [[ $? -ne 0 ]]; then
         all_success=false
     else
         # Copy the generated files to the source directory
-        cp $HW_BUILD_DIR/$(basename $(dirname $path))/*.png $HW_SOURCE_DIR/$path/../
-        cp $HW_BUILD_DIR/$(basename $(dirname $path))/*.pdf $HW_SOURCE_DIR/$path/../
-        cp $HW_BUILD_DIR/$(basename $(dirname $path))/*ibom.html $HW_SOURCE_DIR/$path/../
+        cp $HW_BUILD_DIR/$(basename $(dirname $path))/*.png $HW_SOURCE_DIR/$path/../ > /dev/null 2>&1
+        cp $HW_BUILD_DIR/$(basename $(dirname $path))/*.pdf $HW_SOURCE_DIR/$path/../ > /dev/null 2>&1
+        cp $HW_BUILD_DIR/$(basename $(dirname $path))/*ibom.html $HW_SOURCE_DIR/$path/../ > /dev/null 2>&1
     fi
 done
 

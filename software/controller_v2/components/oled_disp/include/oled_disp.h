1031e33bcbdb6254c9ad7a7e7e0bc573acc4470a
Toon Van Eyck <toonvaneyck@gmail.com>
2025-01-03 23:46:29 +0000

    added oled_disp component

---
+++ software/controller_v2/components/oled_disp/include/oled_disp.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "esp_err.h"
+#include "esp_log.h"
+
+esp_err_t oled_disp_init(void);
---
+++ software/controller_v2/components/oled_disp/test/test.c
@@ -0,0 +1,6 @@
+#include "esp_err.h"
+#include "memory_checks.h"
+#include "oled_disp.h"
+#include "unity.h"
+
+#define TAG "OLED_DISP_TEST"

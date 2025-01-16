445a97332439654023a5d473aa9eed07c4e13781
Toon Van Eyck <toonvaneyck@gmail.com>
2025-01-16 17:17:11 +0000

    rename some vars in ci script

--- hardware/ci-script-gerbolyze.sh
+++ hardware/ci-script-gerbolyze.sh
@@ -20,14 +20,14 @@ if [ "$(hostname)" != "gerbolyze" ]; then
 fi
 
 BASE_DIR=$(git rev-parse --show-toplevel)
-FLAPS_SOURCE_DIR=${BASE_DIR}/hardware/module/flaps
-FLAPS_BUILD_DIR=${BASE_DIR}/build/flaps
+SOURCE_DIR=${BASE_DIR}/hardware/module/flaps
+BUILD_DIR=${BASE_DIR}/build/flaps
 
 # Try generating the flaps
-mkdir -p ${FLAPS_BUILD_DIR}
-rm -rf ${FLAPS_BUILD_DIR}/*
+mkdir -p ${BUILD_DIR}
+rm -rf ${BUILD_DIR}/*
 echo -e "\033[1;36m$message Flaps\033[0m"
 echo -e "\033[96mStep 1: Generate .zip with browser based flap generator\033[0m"
-python3 ${FLAPS_SOURCE_DIR}/browser_based_flap_generator/test/browser_based_flap_generator_test.py ${FLAPS_SOURCE_DIR}/browser_based_flap_generator/index.html ${FLAPS_BUILD_DIR}
+python3 ${SOURCE_DIR}/browser_based_flap_generator/test/browser_based_flap_generator_test.py ${SOURCE_DIR}/browser_based_flap_generator/index.html ${BUILD_DIR}
 echo -e "\033[96mStep 2: Convert .zip with gerbolyze\033[0m"
-${FLAPS_SOURCE_DIR}/svg_to_gerber.sh ${FLAPS_BUILD_DIR}/flaps.zip ${FLAPS_BUILD_DIR}
+${SOURCE_DIR}/svg_to_gerber.sh ${BUILD_DIR}/flaps.zip ${BUILD_DIR}
--- hardware/ci-script-kibot.sh
+++ hardware/ci-script-kibot.sh
@@ -22,8 +22,8 @@ if [ "$(hostname)" != "kibot" ]; then
 fi
 
 BASE_DIR=$(git rev-parse --show-toplevel)
-HW_SOURCE_DIR=${BASE_DIR}/hardware
-HW_BUILD_DIR=${BASE_DIR}/build/hardware
+SOURCE_DIR=${BASE_DIR}/hardware
+BUILD_DIR=${BASE_DIR}/build/hardware
 
 # Declare modules
 declare -A modules=(
@@ -39,8 +39,8 @@ for name in "${!modules[@]}"; do
     echo -e "\033[1;36m$message $name\033[0m"
 
     # Compare title blocks
-    grep -A 8 "(title_block" $(realpath $HW_SOURCE_DIR/$path/*.kicad_sch) > /tmp/sch_title_block.txt
-    grep -A 8 "(title_block" $(realpath $HW_SOURCE_DIR/$path/*.kicad_pcb) > /tmp/pcb_title_block.txt
+    grep -A 8 "(title_block" $(realpath $SOURCE_DIR/$path/*.kicad_sch) > /tmp/sch_title_block.txt
+    grep -A 8 "(title_block" $(realpath $SOURCE_DIR/$path/*.kicad_pcb) > /tmp/pcb_title_block.txt
     echo "- Comparing Title Blocks"
     if ! diff /tmp/sch_title_block.txt /tmp/pcb_title_block.txt --strip-trailing-cr; then
         echo -e "\e[31mThe title blocks are different.\e[0m"
@@ -48,14 +48,14 @@ for name in "${!modules[@]}"; do
     fi
 
     # Run KiBot
-    kibot -c $(realpath $HW_SOURCE_DIR/$path/*.kibot.yaml) -e $(realpath $HW_SOURCE_DIR/$path/*.kicad_sch) -b $(realpath $HW_SOURCE_DIR/$path/*.kicad_pcb) -d $HW_BUILD_DIR/$(basename $(dirname $path)) $kibot_preflight_only
+    kibot -c $(realpath $SOURCE_DIR/$path/*.kibot.yaml) -e $(realpath $SOURCE_DIR/$path/*.kicad_sch) -b $(realpath $SOURCE_DIR/$path/*.kicad_pcb) -d $BUILD_DIR/$(basename $(dirname $path)) $kibot_preflight_only
     if [[ $? -ne 0 ]]; then
         all_success=false
     else
         # Copy the generated files to the source directory
-        cp $HW_BUILD_DIR/$(basename $(dirname $path))/*.png $HW_SOURCE_DIR/$path/../ > /dev/null 2>&1
-        cp $HW_BUILD_DIR/$(basename $(dirname $path))/*.pdf $HW_SOURCE_DIR/$path/../ > /dev/null 2>&1
-        cp $HW_BUILD_DIR/$(basename $(dirname $path))/*ibom.html $HW_SOURCE_DIR/$path/../ > /dev/null 2>&1
+        cp $BUILD_DIR/$(basename $(dirname $path))/*.png $SOURCE_DIR/$path/../ > /dev/null 2>&1
+        cp $BUILD_DIR/$(basename $(dirname $path))/*.pdf $SOURCE_DIR/$path/../ > /dev/null 2>&1
+        cp $BUILD_DIR/$(basename $(dirname $path))/*ibom.html $SOURCE_DIR/$path/../ > /dev/null 2>&1
     fi
 done
 

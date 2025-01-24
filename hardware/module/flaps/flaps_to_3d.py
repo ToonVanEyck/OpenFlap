e5b68c2704fdf45d3f61a42885047fdd7492896f
Toon Van Eyck <toonvaneyck@gmail.com>
2025-01-24 10:44:03 +0000

    fix bug caused by freecad sub-path importing behaviour

--- hardware/module/flaps/flaps_to_3d.py
+++ hardware/module/flaps/flaps_to_3d.py
@@ -36,7 +36,7 @@ def svg_set_path_ids(svg_file):
             "{http://www.inkscape.org/namespaces/inkscape}label"
         ].replace(" ", "_")
         for index, path in enumerate(group.findall(".//svg:path", namespaces)):
-            path.attrib["id"] = f"{label}_{index}"
+            path.attrib["id"] = f"{label}_{index}_"
 
     tree.write(svg_file, pretty_print=True, xml_declaration=True, encoding="UTF-8")
 
@@ -63,6 +63,32 @@ def extrude_path(doc, pathObj, length, reversed=False, offset=0):
     return extrudeObj
 
 
+def cut_path_holes(doc, originalPaths):
+    # When importing an svg into freecad, freecad imports sub-paths as a separate object.
+    # For example, an "O" character will be imported as 2 separate paths.
+    # One for the outer circle and one for the inner circle.
+    # The outer path will be named name__extrusion and the inner paths will be named name_00n_extrusion.
+    # We need to cut the inner paths out of the outer path.
+    bp = BOPFeatures.BOPFeatures(doc)
+    newPaths = []
+    index = 0
+    for obj in originalPaths:
+        if "__" in obj.Name:
+            prefix = obj.Name.split("__")[0]
+            related_objs = [
+                o
+                for o in originalPaths
+                if o.Name.startswith(prefix) and o.Name != obj.Name
+            ]
+            cut_obj = obj
+            for related_obj in related_objs:
+                cut_obj = bp.make_cut([cut_obj.Name, related_obj.Name])
+            cut_obj.Label = f"{prefix}_fixed_{index}"
+            index += 1
+            newPaths.append(cut_obj)
+    return newPaths
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Convert flap SVG files to 3D models using FreeCAD."
@@ -83,6 +109,15 @@ if __name__ == "__main__":
     # Printing the the flaps with 0.16 mm layers will result 5 layers (0.8 mm / 0.16 mm = 5)
     # The colored characters will be printed with 2 layers (0.16 mm * 2 = 0.32 mm)
     # This leaves a solid black layer in the middle.
+
+    # Create the EXTRACT_PATH directory if it does not exist or clear it if it does
+    if not os.path.exists(EXTRACT_PATH):
+        os.makedirs(EXTRACT_PATH)
+    else:
+        for file in os.listdir(EXTRACT_PATH):
+            file_path = os.path.join(EXTRACT_PATH, file)
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
 
     # Extract the SVG files from the zip archive
     with zipfile.ZipFile(args.zip_path, "r") as zip_ref:
@@ -140,10 +175,18 @@ if __name__ == "__main__":
             for path in bot_silk_paths
         ]
 
+        # Some characters Like an "O","B","8" have holes in them. They are imported as separate paths. So we need to cut the inner paths out of the outer path.
+        topExtrudeObjsFixed = cut_path_holes(doc, topExtrudeObjs)
+        botExtrudeObjsFixed = cut_path_holes(doc, botExtrudeObjs)
+
+        print([obj.Name for obj in topExtrudeObjsFixed + botExtrudeObjsFixed])
+
         # Generate a list of cut tools by copying the extruded paths
-        cutToolObjs = [doc.copyObject(obj) for obj in topExtrudeObjs] + [
-            doc.copyObject(obj) for obj in botExtrudeObjs
+        cutToolObjs = [doc.copyObject(obj) for obj in topExtrudeObjsFixed] + [
+            doc.copyObject(obj) for obj in botExtrudeObjsFixed
         ]
+
+        print([obj.Name for obj in cutToolObjs])
 
         # Perform a boolean cut operation to remove the copied paths from the outline
         bp = BOPFeatures.BOPFeatures(doc)
@@ -153,7 +196,7 @@ if __name__ == "__main__":
 
         # Combine the shapes of topExtrudeObjs and botExtrudeObjs
         combinedObj = doc.addObject("Part::Compound", "combined")
-        combinedObj.Links = topExtrudeObjs + botExtrudeObjs + [flapObj]
+        combinedObj.Links = topExtrudeObjsFixed + botExtrudeObjsFixed + [flapObj]
 
         # Recompute the document
         doc.recompute()
